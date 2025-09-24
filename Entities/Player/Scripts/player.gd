0fb69ecfb73aff5bd47670cb055d3378ebcd7f68
timothymckague <163789610+timothymckague@users.noreply.github.com>
2025-09-24 13:15:08 -0700

    Added more Collisions and Details

--- Entities/Player/Scripts/player.gd
+++ Entities/Player/Scripts/player.gd
@@ -89,7 +89,7 @@ var interactCooldown = 0.1;
 func _ready() -> void:
 	Global.setPlayerReference(self)
 	Global.isPlayerInRange = false
-	self.position = Global.wherePlayerShouldSpawn
+	#self.position = Global.wherePlayerShouldSpawn
 
 
 
