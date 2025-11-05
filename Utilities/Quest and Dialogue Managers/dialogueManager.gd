d21de69c850d4a7d2d7f5d0c57f09e298c11a3de
Arturo C-Zacarias <accesareo@gmail.com>
2025-11-04 17:50:23 -0800

    dialogue display handling

---
+++ Utilities/Quest and Dialogue Managers/dialogueManager.gd
@@ -0,0 +1,13 @@
+extends ColorRect
+
+func showDialogue():
+	self.visible = true
+	print("Dialogue opened")
+	
+func hideDialogue():
+	self.visible = false
+	print("Dialogue closed")
+
+# updates the text box until every character in the string gets printed
+# func scrollingText():
+	
