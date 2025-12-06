e94057e0e25f16ab1ab2db5e42180c3961ee3d9d
GLeTan <146773953+GLeTan@users.noreply.github.com>
2025-12-05 16:36:28 -0800

    Edited back singleton for sound manager and reintegrated audio

---
+++ Utilities/Settings/options_menu.gd
@@ -0,0 +1,25 @@
+extends Control
+
+@onready var master       : HSlider = %Master
+@onready var sfx          : HSlider = %SFX
+@onready var apply_button : Button  = %ApplyButton
+
+func _ready() -> void:
+	# init from autoload
+	master.value = SoundManager.get_volume_linear("Master")
+	sfx.value    = SoundManager.get_volume_linear("SFX")
+
+	# live update
+	master.value_changed.connect(func(v):
+		SoundManager.set_volume_linear("Master", v)
+		SoundManager.save_settings()
+	)
+	sfx.value_changed.connect(func(v):
+		SoundManager.set_volume_linear("SFX", v)
+		SoundManager.save_settings()
+	)
+
+	apply_button.pressed.connect(func():
+		SoundManager.save_settings()
+		queue_free()
+	)
